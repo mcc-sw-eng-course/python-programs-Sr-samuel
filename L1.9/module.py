76effb892e2bb484a3317e55ea2eb4eabcf7cb1b
Samuel Solorzano <sr.samuel@gmail.com>
2019-02-14 08:14:31 -0600

    L1.9 moved the main function to a different file Added zip file with the coverage run

--- L1.9/module.py
+++ L1.9/module.py
@@ -20,13 +20,3 @@ def to_roman_format(dec):
                     result += roman[idx]
                     dec -= val
     return result
-
-import random
-def main():
-    decimal = random.randint(1,5000)
-    roman_fmt = to_roman_format(decimal)
-    print(f'The Roman Format of {decimal} is: {roman_fmt}')
-    
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()
