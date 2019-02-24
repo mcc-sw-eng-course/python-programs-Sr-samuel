f119d464f527527bcf22fc8574ed8f7d0bf40f54
Samuel Solorzano <sr.samuel@gmail.com>
2019-02-23 19:41:31 -0600

    L6 Homework Update

--- L6/list_sorter.py
+++ L6/list_sorter.py
@@ -6,11 +6,24 @@ Materia: Analysis, Design, and Construction of Software Systems
 
 Development Exercises - L6
 Create a class in python that Implements a method to sort large amount of data in lists 
-(thousands and millions of items)The data comes into CSV files.
+(thousands and millions of items) 
+The data comes into CSV files.
 """
 
-from enum import Enum
+import csv
+import os
 from pathlib import Path
+from enum import Enum
+from sort_algorithms import mergeSort, quickSort, heapSort
+
+def is_float(value: str) -> bool:
+    return_value = False
+    try:
+        float(value)
+        return_value = True
+    except:
+        return_value = False
+    return return_value
 
 class StatusCode(Enum):
     SUCCESS = 0
@@ -19,33 +32,105 @@ class StatusCode(Enum):
     NOT_A_FILE = 3
     FILE_ERROR = 4
     INVALID_FILE_EXTENSION = 5
+    LIST_IS_EMPTY = 6
+    LIST_ALREADY_SORTED = 7
 
 class ListSorter():
     def __init__(self):
         self.working_list = ()
         self.already_sorted = False
 
-    def set_input_data(self, filename: str):
-        """ Tries to open the file with the given filename and read the values to a list
+    def set_input_data(self, file_path_name: str) -> StatusCode:
+        """ Tries to open the file with the given file_path_name and read the values to a list
         Arguments:
-        filename -- File Name that will be used to create a file
+        file_path_name -- File Name that will be used to create a file
+        Returns a valid StatusCode:
+        SUCCESS     -- Data set from the file stored in the list correctly
+        PARAM_ERROR -- file_path_name was not a valid string
+        NOT_A_FILE  -- Input file_path_name is not a file
+        INVALID_FILE_EXTENSION -- Input file_path_name is not a csv file
         """
-        if isinstance(filename, str):
-            input_file = Path(filename)
+        status_code = StatusCode.FAILURE
+        if isinstance(file_path_name, str):
+            input_file = Path(file_path_name)
             if input_file.is_file():
-                if filename.endswith(".csv"):
-                    with open(filename, "r") as my_file:
-                        # Todo validate that all elements in the file are float numbers
-                        file_content = my_file.read()
-                        self.working_list = [float(x) if x.isdigit() else x for x in file_content.split(',')]
-                        if all(isinstance(x, float) for x in self.working_list):
-                            print("All Float")
+                if file_path_name.endswith(".csv"):
+                    with open(file_path_name, "r") as my_file:
+                        dialect = csv.Sniffer().sniff(my_file.read(4096))
+                        # very simple detection for a valid csv format
+                        if dialect.delimiter != ',':
+                            status_code = StatusCode.FILE_ERROR
+                        else:
+                            my_file.seek(0)
+                            file_content = my_file.read()
+                            # ToDo Check is csv module can be used to parse the whole file...
+                            self.working_list = [float(x) for x in file_content.split(',') if is_float(x)]
+                            status_code = StatusCode.SUCCESS
                 else:
-                    return StatusCode.INVALID_FILE_EXTENSION
+                    status_code = StatusCode.INVALID_FILE_EXTENSION
             else:
-                return StatusCode.NOT_A_FILE
+                status_code = StatusCode.NOT_A_FILE
         else:
-            return StatusCode.PARAM_ERROR
-        pass
+            status_code = StatusCode.PARAM_ERROR
+        return status_code
     
+    def set_output_data(self, file_path_name: str) -> StatusCode:
+        """ Writes the list to the given file_path_name
+        Arguments:
+        file_path_name -- File Name that will be used to create a file
+        Returns a valid StatusCode:
+        SUCCESS     -- File Written correctly
+        PARAM_ERROR -- file_path_name was not a valid string
+        """
+        status_code = StatusCode.FAILURE
+        if isinstance(file_path_name, str):
+            with open(file_path_name, "w") as my_file:
+                csv_writer = csv.writer(my_file, delimiter=',')
+                csv_writer.writerow(self.working_list)
+            status_code = StatusCode.SUCCESS
+        else:
+            status_code = StatusCode.PARAM_ERROR
+        return status_code
 
+    def execute_merge_sort(self):
+        """ It will sort the list using the merge sort.
+        List must not be empty, and must not be previously sorted
+        """
+        status_code = StatusCode.FAILURE
+        if len(self.working_list) > 0:
+            if self.already_sorted == False:
+                mergeSort(self.working_list)
+                self.already_sorted = True
+                status_code = StatusCode.SUCCESS
+            else:
+                status_code = StatusCode.LIST_ALREADY_SORTED
+        else:
+            status_code = StatusCode.LIST_IS_EMPTY
+        return status_code
+
+    def execute_heap_sort(self):
+        if len(self.working_list) > 0:
+            if self.already_sorted == False:
+                heapSort(self.working_list)
+                self.already_sorted = True
+                status_code = StatusCode.SUCCESS
+            else:
+                status_code = StatusCode.LIST_ALREADY_SORTED
+        else:
+            status_code = StatusCode.LIST_IS_EMPTY
+        return status_code
+
+    def execute_quick_sort(self):
+        if len(self.working_list) > 0:
+            if self.already_sorted == False:
+                quickSort(self.working_list)
+                self.already_sorted = True
+                status_code = StatusCode.SUCCESS
+            else:
+                status_code = StatusCode.LIST_ALREADY_SORTED
+        else:
+            status_code = StatusCode.LIST_IS_EMPTY
+        return status_code
+
+    def get_performance_data(self):
+        pass
--- L6/test_list_sorter.py
+++ L6/test_list_sorter.py
@@ -9,8 +9,10 @@ Unit tests for the ListSorter class
 """
 import sys
 sys.path.append('/L6')
+import os
 import unittest
-from list_sorter import ListSorter
+import random
+from list_sorter import *
 
 class ListSorter_test(unittest.TestCase):
     def setUp(self):
@@ -19,10 +21,191 @@ class ListSorter_test(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_listsorter_InputDataValid(self):
+    # def create_csvfile_with_content(self, filename: str, content: list):
+    #     """ Creates a file
+    #     Arguments:
+    #     filename -- File Name that will be used to create a file
+    #     content -- This content will be copied to the file after is created
+    #     """
+    #     if isinstance(filename, str) and isinstance(content, list):
+    #         with open(filename, "w") as my_file:
+    #             file_writer = csv.writer(my_file, delimiter = ',', quoting=csv.QUOTE_NONNUMERIC)
+    #             file_writer.writerow(content)
+
+    def create_csvfile_random_content(self, filename: str, size: int):
+        """ Creates a csv file with random float content
+        Arguments:
+        filename -- File Name that will be used to create a file
+        size -- quantity of float numbers to create
+        """
+        low_limit = 0
+        high_limit = 5000
+        if isinstance(filename, str) and isinstance(size, int):
+            with open(filename, "w") as my_file:
+                file_writer = csv.writer(my_file, delimiter = ',', quoting=csv.QUOTE_NONNUMERIC)
+                file_writer.writerow([random.uniform(low_limit, high_limit) for x in range(size)])
+        
+
+    def create_file_with_content(self, filename: str, content: str):
+        """ Creates a file
+        Arguments:
+        filename -- File Name that will be used to create a file
+        content -- This content will be copied to the file after is created
+        """
+        if isinstance(filename, str) and isinstance(content, str):
+            with open(filename, "w") as my_file:
+                my_file.write(content)
+
+    def delete_file(self, filename: str):
+        os.remove(filename)
+
+    # Tests for "is_float" function
+    def test_isFloat_ValidFloat(self):
+        self.assertTrue(is_float('5.0'))
+
+    def test_isFloat_ValidNegativeFloat(self):
+        self.assertTrue(is_float('-5.0'))
+
+    def test_isFloat_ValidInt(self):
+        self.assertTrue(is_float('5'))
+
+    def test_isFloat_ValidNegativeInt(self):
+        self.assertTrue(is_float('-5'))
+
+    def test_isFloat_NotAFloat(self):
+        self.assertFalse(is_float('abc'))
+
+    # Tests for "set_input_data" function
+    def test_listsorter_InvalidCSVFile(self):
         ls = ListSorter()
-        ls.set_input_data("L6/data1.csv")
-        print(ls.working_list)
+        filename = "InvalidFile.csv"
+        self.create_file_with_content(filename, "123  456   765\n567 876 987 hi")
+        self.assertEqual(StatusCode.FILE_ERROR, ls.set_input_data(filename))
+        self.delete_file(filename)
+
+    def test_listsorter_NotACVSFile(self):
+        ls = ListSorter()
+        filename = "NotACSVFile.txt"
+        self.create_csvfile_random_content(filename, 10)
+        self.assertEqual(StatusCode.INVALID_FILE_EXTENSION, ls.set_input_data(filename))
+        self.delete_file(filename)
+
+    def test_listsorter_NotAFile(self):
+        ls = ListSorter()
+        filename = "IDoNotExist.txt"
+        self.assertEqual(StatusCode.NOT_A_FILE, ls.set_input_data(filename))
+
+    def test_listsorter_InvalidFilename(self):
+        ls = ListSorter()
+        filename = 55
+        self.assertEqual(StatusCode.PARAM_ERROR, ls.set_input_data(filename))
+
+    def test_listsorter_ValidCSVFile(self):
+        ls = ListSorter()
+        filename = "ValidFile.csv"
+        self.create_csvfile_random_content(filename, 50000)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(filename))
+        self.delete_file(filename)
+
+    # Tests for "set_output_data" function
+    def test_listsorter_ValidOutput(self):
+        ls = ListSorter()
+        in_filename = "ValidFile.csv"
+        out_filename = "OutputFile.csv"
+        self.create_csvfile_random_content(in_filename, 50)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(in_filename))
+        self.assertEqual(StatusCode.SUCCESS, ls.set_output_data(out_filename))
+        self.delete_file(in_filename)
+        self.delete_file(out_filename)
+
+    def test_listsorter_InvalidParam(self):
+        ls = ListSorter()
+        out_filename = 55
+        self.assertEqual(StatusCode.PARAM_ERROR, ls.set_output_data(out_filename))
+
+    # testing merge_sort
+    def test_listsorter_MergeSort(self):
+        ls = ListSorter()
+        in_filename = "MergeSort.csv"
+        out_filename = "SortedMergeSort.csv"
+        self.create_csvfile_random_content(in_filename, 5)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(in_filename))
+        self.assertEqual(StatusCode.SUCCESS, ls.execute_merge_sort())
+        self.assertEqual(StatusCode.SUCCESS, ls.set_output_data(out_filename))
+        self.delete_file(in_filename)
+        self.delete_file(out_filename)
+    
+    def test_listsorter_MergeSortAlreadySorted(self):
+        ls = ListSorter()
+        in_filename = "MergeSort.csv"
+        out_filename = "SortedMergeSort.csv"
+        self.create_csvfile_random_content(in_filename, 10)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(in_filename))
+        self.assertEqual(StatusCode.SUCCESS, ls.execute_merge_sort())
+        self.assertEqual(StatusCode.SUCCESS, ls.set_output_data(out_filename))
+        self.assertEqual(StatusCode.LIST_ALREADY_SORTED, ls.execute_merge_sort())
+        self.delete_file(in_filename)
+        self.delete_file(out_filename)
+
+    def test_listsorter_MergeSortEmptyList(self):
+        ls = ListSorter()
+        self.assertEqual(StatusCode.LIST_IS_EMPTY, ls.execute_merge_sort())
+
+    # testing heapSort
+    def test_listsorter_HeapSort(self):
+        ls = ListSorter()
+        in_filename = "HeapSortData.csv"
+        out_filename = "SortedByHeapSort.csv"
+        self.create_csvfile_random_content(in_filename, 5)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(in_filename))
+        self.assertEqual(StatusCode.SUCCESS, ls.execute_heap_sort())
+        self.assertEqual(StatusCode.SUCCESS, ls.set_output_data(out_filename))
+        self.delete_file(in_filename)
+        self.delete_file(out_filename)
+    
+    def test_listsorter_HeapSortAlreadySorted(self):
+        ls = ListSorter()
+        in_filename = "HeapSortData.csv"
+        out_filename = "SortedByHeapSort.csv"
+        self.create_csvfile_random_content(in_filename, 10)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(in_filename))
+        self.assertEqual(StatusCode.SUCCESS, ls.execute_heap_sort())
+        self.assertEqual(StatusCode.SUCCESS, ls.set_output_data(out_filename))
+        self.assertEqual(StatusCode.LIST_ALREADY_SORTED, ls.execute_heap_sort())
+        self.delete_file(in_filename)
+        self.delete_file(out_filename)
+
+    def test_listsorter_HeapSortEmptyList(self):
+        ls = ListSorter()
+        self.assertEqual(StatusCode.LIST_IS_EMPTY, ls.execute_heap_sort())
+
+    # testing quickSort
+    def test_listsorter_QuickSort(self):
+        ls = ListSorter()
+        in_filename = "QuickSortData.csv"
+        out_filename = "SortedByQuickSort.csv"
+        self.create_csvfile_random_content(in_filename, 5)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(in_filename))
+        self.assertEqual(StatusCode.SUCCESS, ls.execute_quick_sort())
+        self.assertEqual(StatusCode.SUCCESS, ls.set_output_data(out_filename))
+        self.delete_file(in_filename)
+        self.delete_file(out_filename)
+    
+    def test_listsorter_QuickSortAlreadySorted(self):
+        ls = ListSorter()
+        in_filename = "QuickSortData.csv"
+        out_filename = "SortedByQuickSort.csv"
+        self.create_csvfile_random_content(in_filename, 10)
+        self.assertEqual(StatusCode.SUCCESS, ls.set_input_data(in_filename))
+        self.assertEqual(StatusCode.SUCCESS, ls.execute_quick_sort())
+        self.assertEqual(StatusCode.SUCCESS, ls.set_output_data(out_filename))
+        self.assertEqual(StatusCode.LIST_ALREADY_SORTED, ls.execute_quick_sort())
+        self.delete_file(in_filename)
+        self.delete_file(out_filename)
+
+    def test_listsorter_QuickSortEmptyList(self):
+        ls = ListSorter()
+        self.assertEqual(StatusCode.LIST_IS_EMPTY, ls.execute_quick_sort())
 
 if __name__ == '__main__':
     unittest.main()
